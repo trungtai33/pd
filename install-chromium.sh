1126086d6cc5541d3a7fea7aceb554915b8bb5a9
trungtai33 <83492345+trungtai33@users.noreply.github.com>
2023-01-25 13:43:18 +0700

    ...

--- install-chromium.sh
+++ install-chromium.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update -y
-sudo apt install software-properties-common -y
+sudo apt install gnupg -y
 sudo echo "deb http://deb.debian.org/debian stable main" > /etc/apt/sources.list.d/debian.sources.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9
