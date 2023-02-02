93c548717322acdb263e1c301e69303f3c3c171a
trungtai33 <83492345+trungtai33@users.noreply.github.com>
2023-02-02 20:36:51 +0700

    ...

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -43,7 +43,7 @@ printf "\n\e[34m[\e[32m*\e[34m]\e[36m Downloading ${distribution}, please wait..
 if ! curl --location --output "${tarball}" \
 	"https://partner-images.canonical.com/core/${version}/current/ubuntu-${version}-core-cloudimg-${arch}-root.tar.gz"; then
 	printf "\e[0m\n\e[34m[\e[31m!\e[34m]\e[31m Download failed, please check your network connection.\e[0m\n\n"
-	rm -f "${PREFIX}/share/${directory}"
+	rm -rf "${PREFIX}/share/${directory}"
 	exit 1
 fi
 printf "\e[0m\n\e[34m[\e[32m*\e[34m]\e[36m Installing ${distribution}, please wait...\e[0m\n"
