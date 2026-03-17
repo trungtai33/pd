85cee1ca154de24e2fd6673408b49f5a97e33923
trungtai33 <83492345+trungtai33@users.noreply.github.com>
2026-03-17 17:42:35 +0700

    ...

--- uninstall-debian.sh
+++ uninstall-debian.sh
@@ -7,7 +7,7 @@ if [ -z "$(command -v dialog)" ]; then
 	printf "\n\e[31mError: 'dialog' is not installed.\e[0m\n\n"
 	exit 1
 fi
-version=$(dialog --title "Debian Uninstaller" --inputbox "Enter the version code name:" 8 40 2>&1 > /dev/tty); clear
+version=$(dialog --title "Debian Uninstaller" --inputbox "Enter the version codename:" 8 40 2>&1 > /dev/tty); clear
 if [ -z "${version}" ]; then
 	exit 1
 fi
